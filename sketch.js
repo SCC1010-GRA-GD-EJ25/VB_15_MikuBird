4e79d112119f65337632929482e6f1c8c793ced3
Víctor Manuel Bátiz Beltrán <51245128+vbatiz@users.noreply.github.com>
2025-03-11 15:09:24 -0700

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function preload() {
+  // put preload code here
+}
+
+function setup() {
+  // put setup code here
+  createCanvas(1000,512)
+}
+
+function draw() {
+  // put drawing code here
+  background(255)
+  rect(100,100,200,200)
+}
