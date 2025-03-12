2e1aa80d0c3ae17532c07f5ab3b25cd7eb6e5111
Víctor Manuel Bátiz Beltrán <51245128+vbatiz@users.noreply.github.com>
2025-03-12 22:56:39 +0000

    Se agregaron obstáculos

--- sketch.js
+++ sketch.js
@@ -1,15 +1,23 @@
 let imagenFondo
 let imagenInicio
 let personaje
+let pared
 let x = 0
 let posY = 100
 let dY = 3
 let estado = 0 //0: menú; 1: jugando; 2: gameOver
+let wallX = []
+let wallY = []
+let puntaje = 0
+let puntajeMax = 0
+let recordAnterior = 0
 
 function preload() {
   // put preload code here
   imagenFondo = loadImage("./images/fondojuego00.png")
+  imagenInicio = loadImage("./images/fondoinicio00.jpg")
   personaje = loadImage("./images/miku00.gif")
+  pared = loadImage("./images/pared.png")
 }
 
 function setup() {
@@ -21,6 +29,7 @@ function setup() {
 function draw() {
   // put d(rawing code here
   if (estado === 1) {
+    imageMode(CORNER)
     background(255)
     image(imagenFondo,x,0)
     image(imagenFondo, x + imagenFondo.width, 0)
@@ -30,13 +39,37 @@ function draw() {
     if (x <= -imagenFondo.width) {
       x = 0
     }
+    //Obstáculos
+    for (let i=0; i<wallX.length; i++) {
+      imageMode(CENTER)
+      image(pared,wallX[i],wallY[i]-500)
+      image(pared,wallX[i],wallY[i]+500)
+
+      if (wallX[i] < 0) {
+        wallX[i] = width
+        wallY[i] = random(200,300)
+      }
+
+      //Puntaje
+      if (wallX[i] === 100) {
+        puntaje = puntaje + 1
+        puntajeMax = max(puntajeMax,puntaje)
+      }
+
+      wallX[i] = wallX[i] - 5  //Para que se muevan los obstáculos
+
+    }
+
+    //Personaje
     image(personaje, 100, posY,60,60)
+    text("Puntaje: " + puntaje, width/2-60,30)
   } else if (estado === 0) {
     cursor()
-    image(imagenFondo, 0, 0)
-    textSize(50)
+    image(imagenInicio, 0, 0,450,600)
+    textSize(24)
     fill(255)
-    text("Haga clic para comenzar", 100, 100)
+    text("Puntaje Máximo " + puntajeMax, 600, 100)
+    text("Haga clic para comenzar", 600, 200)
   }
 }
 
@@ -47,6 +80,12 @@ function mousePressed() {
     posY = 100
     x = 0
     dY = 3
+    wallX = [500, 800,1100]
+    wallY[0] = random(200,300)
+    wallY[1] = random(200,300)
+    wallY[2] = random(200,300)
+    puntaje = 0
+    recordAnterior = puntajeMax
     noCursor()
   }
   dY = -15
